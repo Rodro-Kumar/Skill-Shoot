1133085d936013935f4d140422e1e702d0ac535f
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-08-19 12:38:00 +0600

    hero section complete

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
+import Hero from "./Hero";
 
 const Banner = () => {
   return (
     <div className="bg-primaryColor">
       <div className="container">
         <Nav />
+        <Hero />
       </div>
     </div>
   );
