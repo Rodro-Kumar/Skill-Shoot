51400fdd002f6a73cb9eee1851dabf61aa7a1dbd
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-09-08 18:21:30 +0600

    validation

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import lineArt from "../assets/Footer/artLine.png";
 import roundArt from "../assets/Footer/roundArt.png";
+import { MdOutlineErrorOutline } from "react-icons/md";
 
 const Footer = () => {
+  const [inputData, setinputData] = useState({
+    email: "",
+  });
+
+  const [errorInputData, seterrorInputData] = useState({
+    emailError: "",
+  });
+
+  const HandleINput = (event) => {
+    setinputData({
+      ...inputData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const HandleSubscribe = () => {
+    const { email } = inputData;
+    if (!email) {
+      seterrorInputData({
+        ...errorInputData,
+        emailError: "Please enter your email",
+      });
+    } else {
+      seterrorInputData({
+        ...errorInputData,
+        emailError: "",
+      });
+    }
+  };
+
+  console.log(errorInputData);
+
   return (
     <div className="bg-primaryColor pt-[100px]">
       <div className="container">
@@ -18,15 +51,39 @@ const Footer = () => {
             By subscribing with your mail, you will accept our privacy policy
           </p>
           <div className="flex items-center gap-x-4 pt-16">
-            <input
-              type="text"
-              placeholder="Enter your email"
-              className="pl-[30px] pr-[220px] py-4 rounded placeholder:text-base placeholder:font-poppins placeholder:text-whites placeholder:font-normal bg-[#ffffff1f] text-whites text-base"
-            />
-            <button className="text-[18px] font-poppins font-medium text-whites px-6 rounded bg-secondColor py-[14px] hover:bg-[#db906b] transition-all">
+            <div className="w-[400px]">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                className={`pl-[30px] pr-[30px] w-full py-4 rounded placeholder:text-base placeholder:font-poppins placeholder:text-whites placeholder:font-normal bg-[#ffffff1f] text-whites text-base ${
+                  errorInputData.emailError
+                    ? "border-red-600 border-[1px]"
+                    : "border-none"
+                }`}
+                onChange={HandleINput}
+              />
+            </div>
+
+            <button
+              onClick={HandleSubscribe}
+              className="text-[18px] font-poppins font-medium text-whites px-6 rounded bg-secondColor py-[14px] hover:bg-[#db906b] transition-all"
+            >
               Subscribe us
             </button>
           </div>
+          <div className="pt-2 xl:mr-[30%]">
+            {errorInputData.emailError ? (
+              <p className="text-red-600 flex items-center gap-x-1 font-poppins font-normal text-base">
+                <MdOutlineErrorOutline />
+                {errorInputData.emailError}
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+
           {/* round art */}
           <div className="absolute bottom-[160px] right-0">
             <img src={roundArt} alt="" />
