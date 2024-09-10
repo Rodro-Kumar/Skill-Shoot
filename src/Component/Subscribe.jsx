1dd111bf955e1c253a9c2879a46cca11186e2b3d
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-09-10 13:19:32 +0600

    responsive

--- src/Component/Subscribe.jsx
+++ src/Component/Subscribe.jsx
@@ -3,29 +3,29 @@ import vector from "../assets/Subscribe/vector.png";
 
 const Subscribe = () => {
   return (
-    <div id="Subscribe" className="mb-[100px]">
+    <div id="Subscribe" className="mb-[50px] lg:mb-[100px] px-4 xl:px-0">
       <div className="container">
-        <div className="bg-thirdColor py-[70px] px-16 rounded-2xl relative">
+        <div className="bg-thirdColor py-14 lg:py-[70px] px-16 rounded-2xl relative">
           {/* round shap */}
           <div className="overflow-hidden w-[80px] h-[80px] absolute top-0 left-0 rounded-2xl">
-            <div className="w-[73px] h-[73px] bg-primaryColor rounded-full absolute top-[-11px] left-[-9px]"></div>
+            <div className="lg:w-[73px] w-[60px] h-[60px] lg:h-[73px] bg-primaryColor rounded-full absolute top-[-11px] left-[-9px]"></div>
           </div>
 
           {/* content */}
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col lg:flex-row justify-between items-center">
             <div className="max-w-[630px]">
-              <h5 className="text-4xl font-semibold font-poppins w-full leading-[50px]">
+              <h5 className="text-[18px] leading-[25px] lg:text-4xl font-semibold font-poppins w-full lgleading-[50px]">
                 <span className="text-secondColor">Improve</span>{" "}
                 <span className="text-blacks">
                   your skills, and reach your career as soon as possible
                 </span>
               </h5>
             </div>
-            <div className="flex items-center gap-x-5">
-              <button className="py-[15px] px-[25px] rounded bg-secondColor text-whites capitalize font-poppins font-bold text-base hover:bg-[#b97959] transition-all cursor-default">
+            <div className="flex items-center gap-x-5 pt-3 lg:pt-0">
+              <button className="lg:py-[15px] py-3 px-4 lg:px-[25px] rounded bg-secondColor text-whites capitalize font-poppins font-bold lg:text-base text-sm whitespace-nowrap hover:bg-[#b97959] transition-all cursor-default">
                 Join Now
               </button>
-              <button className="py-[15px] px-[25px] rounded bg-primaryColor text-whites capitalize font-poppins font-bold text-base hover:bg-[#2b6e60] transition-all cursor-default">
+              <button className="lg:py-[15px] py-3 px-4 lg:px-[25px] rounded bg-primaryColor text-whites capitalize font-poppins font-bold lg:text-base text-sm whitespace-nowrap hover:bg-[#2b6e60] transition-all cursor-default">
                 Subscribe Course
               </button>
             </div>
