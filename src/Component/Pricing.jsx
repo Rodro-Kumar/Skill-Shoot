fd45f4314db0f26cb3c9d1871362d8eeeeeb2629
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-09-13 21:19:28 +0600

    responsive

--- src/Component/Pricing.jsx
+++ src/Component/Pricing.jsx
@@ -4,32 +4,34 @@ import disignVector from "../assets/pricing_design.png";
 
 const Pricing = () => {
   return (
-    <div className="pb-[100px]">
+    <div className="pb-[85px] lg:pb-[100px] px-4 xl:px-0">
       <div className="container">
         <div className="flex items-center justify-center flex-col text-center">
-          <h4 className="text-4xl font-poppins font-semibold pb-[10px]">
+          <h4 className="text-[18px] lg:text-4xl font-poppins font-semibold pb-[10px]">
             <span className="text-secondColor">Subscribe</span>{" "}
             <span className="text-blacks">with us now</span>
           </h4>
-          <p className="text-base font-poppins font-normal text-paraGraphColor max-w-[545px] leading-[30px]">
+          <p className="text-xs lg:text-base font-poppins font-normal text-paraGraphColor max-w-[545px] leading-[20px] lg:leading-[30px]">
             by subscribing now you will be able to access the material easily
             and cheaply, so you will be very efficient and benefit
           </p>
         </div>
         {/* price */}
-        <div className="flex gap-x-[65px] pt-[50px] justify-center items-center">
+        <div className="flex flex-col gap-x-4 sm:flex-row gap-y-20 lg:gap-x-[65px] pt-[50px] justify-center items-center">
           {/* Base */}
           <div className="max-w-[227px] h-[403px]">
             <h5 className="font-poppins pb-1">
-              <span className="text-blacks text-4xl font-bold">$50</span>
+              <span className="text-blacks text-xl lg:text-4xl font-bold">
+                $50
+              </span>
               <span className="text-base font-normal text-paraGraphColor">
                 / 1 month
               </span>
             </h5>
-            <h4 className="font-poppins font-bold text-blacks text-[28px]">
+            <h4 className="font-poppins font-bold text-blacks text-xl lg:text-[28px]">
               Base
             </h4>
-            <p className="font-poppins text-paraGraphColor font-normal text-base pt-1">
+            <p className="font-poppins text-paraGraphColor font-normal text-sm lg:text-base pt-1">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </p>
@@ -77,34 +79,35 @@ const Pricing = () => {
               </div>
             </div>
             {/* Choose plan */}
-            <button className="group py-3 text-secondColor text-base font-poppins font-semibold rounded-3xl bg-thirdColor w-full hover:bg-secondColor transition-all hover:text-whites">
+            <button className="group py-2 lg:py-3 text-secondColor text-sm lg:text-base font-poppins font-semibold rounded-3xl bg-thirdColor w-full hover:bg-secondColor transition-all hover:text-whites">
               Choose plan
             </button>
           </div>
           {/* Pro */}
-
           <div className="max-w-[292px] z-40 relative rounded-[28px] bg-primaryColor shadow-2xl shadow-primaryColor">
             <div className="absolute top-0 right-[-35px] -z-10">
               <img src={disignVector} alt={disignVector} />
             </div>
             {/* Most Popular */}
             <div className="flex items-end justify-end pt-5 pr-5">
-              <div className="font-poppins font-semibold text-whites uppercase py-2 px-4 rounded-2xl bg-secondColor tracking-[03px]">
+              <div className="font-poppins font-semibold text-whites uppercase py-2 lg:px-4 px-2 rounded-2xl bg-secondColor tracking-[03px] text-xs lg:text-sm">
                 MOST POPULAR
               </div>
             </div>
             {/* Content */}
-            <div className="px-[30px] pb-[30px] text-whites">
+            <div className="px-5 lg:px-[30px] pb-5 lg:pb-[30px] text-whites">
               <h5 className="font-poppins pb-3 pt-4">
-                <span className="text-whites text-4xl font-bold">$100</span>
-                <span className="text-base font-normal text-whites">
+                <span className="text-whites text-xl lg:text-4xl font-bold">
+                  $100
+                </span>
+                <span className="text-sm lg:text-base font-normal text-whites">
                   / 6 month
                 </span>
               </h5>
-              <h4 className="font-poppins font-bold text-whites text-[28px]">
+              <h4 className="font-poppins font-bold text-whites text-xl lg:text-[28px]">
                 Base
               </h4>
-              <p className="font-poppins text-whites font-normal text-base pt-1">
+              <p className="font-poppins text-whites font-normal text-sm lg:text-base pt-1">
                 Lorem Ipsum is simply dummy text of the printing
               </p>
               {/* features */}
@@ -151,7 +154,7 @@ const Pricing = () => {
                 </div>
               </div>
               {/* Choose plan */}
-              <button className="group py-3  text-base font-poppins font-semibold rounded-3xl w-full bg-secondColor transition-all text-whites hover:bg-[#e0926b]">
+              <button className="group py-2 lg:py-3 text-sm lg:text-base font-poppins font-semibold rounded-3xl w-full bg-secondColor transition-all text-whites hover:bg-[#e0926b]">
                 Choose plan
               </button>
             </div>
@@ -160,15 +163,17 @@ const Pricing = () => {
           {/* Enterprise */}
           <div className="max-w-[227px] h-[403px]">
             <h5 className="font-poppins pb-1">
-              <span className="text-blacks text-4xl font-bold">$200</span>
+              <span className="text-blacks text-xl lg:text-4xl font-bold">
+                $200
+              </span>
               <span className="text-base font-normal text-paraGraphColor">
                 / 12 month
               </span>
             </h5>
-            <h4 className="font-poppins font-bold text-blacks text-[28px]">
+            <h4 className="font-poppins font-bold text-blacks text-xl lg:text-[28px]">
               Base
             </h4>
-            <p className="font-poppins text-paraGraphColor font-normal text-base pt-1">
+            <p className="font-poppins text-paraGraphColor font-normal text-sm lg:text-base pt-1">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </p>
@@ -216,7 +221,7 @@ const Pricing = () => {
               </div>
             </div>
             {/* Choose plan */}
-            <button className="group py-3 text-secondColor text-base font-poppins font-semibold rounded-3xl bg-thirdColor w-full hover:bg-secondColor transition-all hover:text-whites">
+            <button className="group py-2 lg:py-3 text-secondColor text-sm lg:text-base font-poppins font-semibold rounded-3xl bg-thirdColor w-full hover:bg-secondColor transition-all hover:text-whites">
               Choose plan
             </button>
           </div>
